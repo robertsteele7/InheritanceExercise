8dbffd189571817f3cc56bcfcfbba1ce7980446a
robertsteele7 <atpcycle7@gmail.com>
2022-10-08 00:42:36 -0500

    upload

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -8,32 +8,50 @@ namespace Inheritance
         {
             // TODO Be sure to follow best practice when creating your classes
 
-            // Create a class Animal
+            // Done-Create a class Animal
             // give this class 4 members that all Animals have in common
-
-
-            // Create a class Bird
+            // Done-Create a class Bird
             // give this class 4 members that are specific to Bird
             // Set this class to inherit from your Animal Class
-
-            // Create a class Reptile
+            // Done-Create a class Reptile
             // give this class 4 members that are specific to Reptile
             // Set this class to inherit from your Animal Class
-
-
-
 
             /*Create an object of your Bird class
              *  give values to your members using the object of your Bird class
              *  
              * Creatively display the class member values 
              */
+            var myBird = new Bird();
+            myBird.WingColor = "blue";
+            myBird.CanFly = true;
+            myBird.DoMigrate = true;
+            myBird.BeakLength = 3.5;
+
 
             /*Create an object of your Reptile class
              *  give values to your members using the object of your Reptile class
              *  
              * Creatively display the class member values 
              */
+            var lizard = new Reptile()
+            {
+                IsColdBlooded = true,
+                IsScaly = true,
+                Habitat = "swamp",
+                CanGrowTail = true
+            };
+            var myAnimals = new Animal[] {myBird, lizard };
+
+            foreach (var animal in myAnimals)
+            { 
+                Console.WriteLine($"Alive:{animal.IsAlive}");
+                Console.WriteLine($"Age:{animal.Age} years old");
+                Console.WriteLine($"It has: {animal.LegCount} legs");
+                Console.WriteLine($"It lives by :{animal.LandSeaAir}");
+                Console.WriteLine($"");
+            }
+            
         }
     }
 }
